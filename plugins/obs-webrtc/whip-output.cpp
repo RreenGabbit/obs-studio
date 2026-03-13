b9c231bfce83630fe0284cbb12c0415701f983c2
obsfix <obsfix@local>
2026-03-13 15:32:06 +0100

    Revert "obs-webrtc: increase WHIP NVENC VBV window"
    This reverts commit 71aab1aaea163d6583f9756ae23057b13ad13756.

--- plugins/obs-webrtc/whip-output.cpp
+++ plugins/obs-webrtc/whip-output.cpp
@@ -785,7 +785,7 @@ void WHIPOutput::ApplyWhipEncoderOverrides()
 		if (bitrate_kbps <= 0)
 			continue;
 
-		const uint64_t vbv_bits = static_cast<uint64_t>(bitrate_kbps) * 375;
+		const uint64_t vbv_bits = static_cast<uint64_t>(bitrate_kbps) * 250;
 		std::string opts = state.opts;
 		if (!opts.empty())
 			opts += " ";
